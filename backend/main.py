2c23d29090d95075b73240d3f72d5cead88b7d90
Utkarshs9924 <utkarshs1199@gmail.com>
2026-03-13 15:04:34 -0700

    Add password hashing with bcrypt for user signup

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from database import get_db_connection
+from passlib.context import CryptContext
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class SignupRequest(BaseModel):
+    name: str
+    email: str
+    password: str
+
 
 @app.get("/")
 def root():
     return {"message": "Yelp Prototype API Running"}
+
 
 @app.get("/test-db")
 def test_db():
@@ -15,3 +25,20 @@ def test_db():
     databases = cursor.fetchall()
     conn.close()
     return {"databases": databases}
+
+
+@app.post("/signup")
+def signup(user: SignupRequest):
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    hashed_password = pwd_context.hash(user.password)
+
+    query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+    cursor.execute(query, (user.name, user.email, hashed_password))
+
+    conn.commit()
+    conn.close()
+
+    return {"message": "User created successfully"}
